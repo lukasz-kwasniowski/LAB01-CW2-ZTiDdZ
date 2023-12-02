8c22aca8ea3ff1246b5e37658ebc3ab290acfb58
lukasz-kwasniowski <lukasz.kwasniowski02@gmail.com>
2023-12-02 14:29:23 +0100

    adding comments

--- Index.js
+++ Index.js
@@ -1,12 +1,19 @@
+
+//importowanie modułów
 const express = require("express");
 var path = require('path');
 
+//inicjalizacja aplikacji express
 const app = express();
 
-function authentication(req, res, next) {
+//tworzenie funkcji do uwierzytelniania
+function authentication(req, res, next) 
+{
+    //Logika uwierzytelniania
 	var authheader = req.headers.authorization;
 	console.log(req.headers);
 
+    //sprawdzenie nagłówka autoryzacji
 	if (!authheader) {
 		var err = new Error('You are not authenticated!');
 		res.setHeader('WWW-Authenticate', 'Basic');
@@ -14,11 +21,14 @@ function authentication(req, res, next) {
 		return next(err)
 	}
 
+    //dedykowanie danych uwierzytelniających
 	var auth = new Buffer.from(authheader.split(' ')[1],
 	'base64').toString().split(':');
 	var user = auth[0];
 	var pass = auth[1];
 
+
+    //sprawdzanie poprawności danych
 	if (user == '' && pass == '') {
 
 		// If Authorized user
